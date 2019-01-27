ec36af3c88aa3d17a321a2d684ca594518973632
ShuDiamonds <fukudaubuntu@gmail.com>
2019-01-27 11:42:05 +0900

    reviewed code

--- Sensing&pandasAggregate.py
+++ Sensing&pandasAggregate.py
@@ -22,11 +22,26 @@ import pandas as pd
 
 from PCF8591 import PCF8591
 
-def sensordatawrite(path,sensordate):
-    now=date.today()
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+
+
+def sensordatawrite(path,sensordata,worksheet):
+    now=datetime.now()
     tmp=[now.year, now.month, now.day, now.hour,sensordata]
-    with open(path, mode='a') as f:
-        f.write(",".join(map(str,tmp)))
+    
+    try:
+        #write csv
+        with open(path, mode='a') as f:
+            f.write(",".join(map(str,tmp)))
+        
+        #write spread sheet
+            worksheet.append_row(tmp)
+    except ZeroDivisionError as e:
+        print(e)
+        
     return
 
 Sensor1_outputpath="Sensor1_ourput.csv"
@@ -34,9 +49,19 @@ Sensor2_outputpath="Sensor2_ourput.csv"
 
 if __name__ == "__main__":
     
+    #init pct8591
     pcf8591 = PCF8591(0x48)
     MotionSensor_pin = 1 #wiringpi number see :https://projects.drogon.net/raspberry-pi/wiringpi/pins/
     wiringpi.pinMode( MotionSensor_pin,0)# set as input pin
+    
+    
+    #init google spreadsheet
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('spreadsheet.json', scope)
+    gc = gspread.authorize(credentials)
+    workbook1 = gc.open('Sensor1_output')
+    workbook2 = gc.open('Sensor2_output')
+    worksheet1 = workbook1.sheet1
+    worksheet2 = workbook2.sheet1
     
     csv_colmnsname=["year","month","hour","sensor_value"]
     #Init outputfile
@@ -57,24 +82,25 @@ if __name__ == "__main__":
             sensordata2=[]
             StartTimeH=datetime.now()
             while True: #1hour loop
-                #check pass 1hour or not 
-                if (datetime.now()-StartTimeH).total_seconds()>3600:
+                #check pass 5min or not 
+                if (datetime.now()-StartTimeH).total_seconds()>30:
                     break
                 # read infrared sensor value
                 sensordata1.append(wiringpi.digitalRead(MotionSensor_pin))
+                print("1:"+str(wiringpi.digitalRead(MotionSensor_pin)))
             
                 #PCF8591 processing AD conversion
                 value=pcf8591.analogRead0()
                 sensordata2.append(value)
                 pcf8591.DAoutput(value)
-                #print(value)
+                print("2:"+str(value))
                 time.sleep(1)
                 
             #### END 1hour loop ####
             sensordata1Series=pd.Series(sensordata1)
             sensordata2Series=pd.Series(sensordata2)
             
-            sensordatawrite(Sensor1_outputpath,sensordata1Series.mean())
+            sensordatawrite(Sensor1_outputpath,sensordata1Series.mean(),worksheet1)
             sensordatawrite(Sensor2_outputpath,sensordata2Series.mean())
             
                 
